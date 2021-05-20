91a830c75d59423695e1d4b37e011e77a3ba1bb3
Jigar Mav <jbhanushali237@gmail.com>
2021-05-20 23:56:30 +0530

    TypeORM added to entity

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,13 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+// reused by gql and orm at same time
+
+// for graphql
 @ObjectType()
+// for typeORM
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
+
   @Field((type) => String)
+  @Column()
   name: string;
+
   @Field((type) => Boolean)
+  @Column()
   isVegan: boolean;
+
   @Field((type) => String)
+  @Column()
   address: string;
+
   @Field((type) => String)
+  @Column()
   ownerName: string;
 }
